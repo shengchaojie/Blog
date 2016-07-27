34c8b2df2cae8f97401a9e62aad74374d3288819
shengchaojie <shengchaojie@163.com>
2016-07-28 01:02:11 +0800

    add log

--- Service/src/test/java/com/scj/service/test/TalkServiceTest.java
+++ Service/src/test/java/com/scj/service/test/TalkServiceTest.java
@@ -2,6 +2,8 @@ package com.scj.service.test;
 
 import com.scj.user.service.TalkService;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 import javax.sound.midi.Soundbank;
@@ -10,6 +12,7 @@ import javax.sound.midi.Soundbank;
  * Created by shengchaojie on 2016/7/17.
  */
 public class TalkServiceTest extends TestBase{
+    private static final Logger LOGGER = LoggerFactory.getLogger(TalkServiceTest.class);
 
     @Resource
     private TalkService talkService;
@@ -18,5 +21,13 @@ public class TalkServiceTest extends TestBase{
     public void getTalks()
     {
         talkService.getTalks(9).stream().forEach(System.out::println);
+    }
+
+    @Test
+    public void testLog()
+    {
+        LOGGER.info("123");
+        LOGGER.debug("debug 123");
+        LOGGER.error("error 123");
     }
 }
